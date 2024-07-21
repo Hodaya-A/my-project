7058004a8beb6e4aa3335491a2c29ffea4b70205
Hodaya-A <hodaya3avraham@gmail.com>
2024-07-21 21:54:58 +0300

    trending update

--- index.js
+++ index.js
@@ -121,12 +121,12 @@ function displayRecommendedProductsByCategory(category, products) {
 
 function loadTrendingCategories() {
     const trendCategories = [
-        { name: "Fashion", image: "https://img.ltwebstatic.com/images3_abc/2024/04/23/f4/1713878671709b75008cd9ba9b0c236d772ba81fe0.png", subCategories: ["mens-shirts", "mens-shoes", "mens-watches", "womens-bags", "womens-dresses", "womens-jewellery", "womens-shoes", "womens-watches"] },
-        { name: "Beauty & Care", image: "https://img.ltwebstatic.com/images3_ccc/2023/11/01/b7/169883112225bb2788cef5889939f5ebc49332471f.jpg", subCategories: ["beauty", "skin-care", "fragrances"] },
-        { name: "Technology & Electronics", image: "https://img.ltwebstatic.com/images3_ccc/2024/03/19/c4/1710828286f5c9e2a717be0ffdb1eb455bcd34bedd.png", subCategories: ["laptops", "smartphones", "tablets", "mobile-accessories"] },
-        { name: "Home & Garden", image: "https://img.ltwebstatic.com/images3_abc/2024/07/15/a0/1721029170c5cbc73e991f39abef682f5f446b335a.png", subCategories: ["furniture", "home-decoration", "kitchen-accessories"] },
-        { name: "Leisure & Sports", image: "https://img.ltwebstatic.com/images3_abc/2024/06/22/ee/171905991293b81a3d93085e4784f56ec82ff55b9d.png", subCategories: ["sports-accessories", "motorcycle", "vehicle"] },
-        { name: "Grocery & Food", image: "https://img.ltwebstatic.com/images3_abc/2024/06/20/a2/17188688111b1d20c11de46f093c1b7551f7b5585c.png", subCategories: ["groceries"] }
+        { name: "Fashion", image:"fashion.jpg", subCategories: ["mens-shirts", "mens-shoes", "mens-watches", "womens-bags", "womens-dresses", "womens-jewellery", "womens-shoes", "womens-watches"] },
+        { name: "Beauty & Care", image: "skin-care.jpg", subCategories: ["beauty", "skin-care", "fragrances"] },
+        { name: "Technology & Electronics", image: "technology.jpg", subCategories: ["laptops", "smartphones", "tablets", "mobile-accessories"] },
+        { name: "Home & Garden", image: "home.jpg", subCategories: ["furniture", "home-decoration", "kitchen-accessories"] },
+        { name: "Leisure & Sports", image: "sports.jpg", subCategories: ["sports-accessories", "motorcycle", "vehicle"] },
+        { name: "Grocery & Food", image: "Grocery.jpg", subCategories: ["groceries"] }
     ];
 
     const trendCategoriesContainer = document.querySelector('.trend-categories');
